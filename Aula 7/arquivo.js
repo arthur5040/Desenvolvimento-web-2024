65f1caecc6e9ef10e7e378a892a0e6b6206e2666
PEDRO DOS SANTOS CARLOS DA SILVA <202401000493@alunos.ibmec.edu.br>
2024-10-28 11:18:16 -0300

    botafogo

---
+++ Aula 7/arquivo.js
@@ -0,0 +1,20 @@
+const container = document.getElementById("container");
+
+let conteudo = ''
+
+const montaCard = (atleta) => `
+    <article>
+    <h1>${atleta.nome}</h1>
+    <img src=${atleta.imagem}>
+    <p>${atleta.detalhes}</p>
+    </article>
+`
+/*for (let i = 0; i < dados.length; i++){
+    let atleta =dados[i];
+
+    conteudo += montaCard(atleta);
+}*/
+
+dados.forEach((atleta) => conteudo += montaCard(atleta))
+
+container.innerHTML = conteudo;
